73253ea07a7c9a252235e62c34383bdd9ef9de47
Alphie Shikwambana <alphie.shikwambana@neutrinos.co>
2020-12-25 02:35:20 +0200

    added a styling guide component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 const routes: Routes = [
-  { component: HomeComponent, path: ''}
+  { component: HomeComponent, path: ''},
+  { component: LayoutComponent, path: 'style'}
 ];
 
 @NgModule({
